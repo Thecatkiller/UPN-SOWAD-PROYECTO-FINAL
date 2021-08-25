51270a5e8a9724055b3d06554f45fcbd391e549f
Diego Calderon <apahisfree@gmail.com>
2021-08-24 23:21:13 -0500

    first commit

---
+++ src/main/java/pe/amadeo/mozart/WebColegioAmadeoMozartApplication.java
@@ -0,0 +1,13 @@
+package pe.amadeo.mozart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebColegioAmadeoMozartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebColegioAmadeoMozartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/amadeo/mozart/WebColegioAmadeoMozartApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.amadeo.mozart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebColegioAmadeoMozartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
